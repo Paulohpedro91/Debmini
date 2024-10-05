9dcc9154e3dedfadf7cf21505df53d8f1510c690
Paulo Henrique <67978903+Paulohpedro91@users.noreply.github.com>
2024-10-04 22:46:29 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# Uso: Debimini
+# Instalação do ambiente gráfico Xfce4 mínimo.
+# Autor: Paulo Henrique Pedro
+# Versão: 0.1
+# Licença: GPL
+# ----------------------------------------------------------------------------
+apt install -y\
+    xorg\
+    build-essential\
+    libxfce4ui-utils \
+    thunar \
+    xfce4-appfinder \
+    xfce4-panel \
+    xfce4-pulseaudio-plugin \
+    xfce4-whiskermenu-plugin \
+    xfce4-session \
+    xfce4-settings \
+    xfce4-terminal \
+    xfconf \
+    xfdesktop4 \
+    xfwm4 \
+    adwaita-qt \
+    firefox-esr \    
